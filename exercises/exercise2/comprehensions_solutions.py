f680d802d53c2c06fc118adbce408f159734e4bf
07734willy <piortau@gmail.com>
2019-04-09 23:33:12 -0500

    added solutions

--- exercises/exercise2/comprehensions_solutions.py
+++ exercises/exercise2/comprehensions_solutions.py
@@ -10,6 +10,8 @@ def make_dict(keys, values):
 def make_dict(kets, values):
 	return {keys[i]:values[i] for i in range(len(keys))}
 
+def make_dict(keys, values):
+    return {key, value for key, value in zip(keys, values)}
 
 #2
 def large_elements(matrix):
@@ -23,7 +25,7 @@ def large_elements(matrix):
 #answer
 def large_elements(matrix):
 	return [elem for row in matrix for elem in row if elem > 4]
-
+# nothing to improve
 
 #3
 def matrix_transpose(matrix):
@@ -38,6 +40,14 @@ def matrix_transpose(matrix):
 def matrix_transpose(matrix):
 	return list([[matrix[0][i], matrix[1][i]] for i in range(len(matrix[0]))])
 
+def matrix_transpose(matrix):
+    return [list(tup) for tup in zip(matrix[0], matrix[1])]
+
+# OR
+
+def matrix_transpose(matrix):
+    return [list(tup) for tup in zip(*matrix)]
+
 
 #4
 def max_every_other(numbers):
@@ -49,6 +59,11 @@ def max_every_other(numbers):
 #answer
 def max_every_other(numbers):
 	return [max(numbers[i], numbers [i+1]) for i in range(0, len(numbers),2)]
+
+
+def max_every_other(numbers):
+    return [max(pair) for pair in zip(numbers[::2], numbers[1::2])]
+
 
 #5
 def repeat_elements(counts):
@@ -63,6 +78,7 @@ def repeat_elements(counts):
 #answer
 def repeat_elements(counts):
 	return [[count] * count for count in counts]	
+#nothing to improve
 
 #6
 def power_of_twos():
@@ -74,9 +90,10 @@ def power_of_twos():
 #answer
 def power_of_twos():
 	yield (2 ** x for x in itertools.count(0,1))
+#nothing to improve
 
 #7
 
 # I don't really understand #7 :( 
 
-
+"\n".join(", ".join(str(e) for e in row) for row in matrix)
